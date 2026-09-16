309e14bbea2a85c6daf25b133736ec2129fa388c
miguelr1881 <93629251+miguelr1881@users.noreply.github.com>
2026-09-16 16:44:07 -0600

    Add DEX privacy policy

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 const CACHE = 'dex-pages-shell-v1';
-const SHELL = ["./", "./index.html", "./portal.css", "./manifest.webmanifest", "./app.css", "./app.js", "./i18n.js", "./sheets-reader.js", "./portal.js", "./lucide.js", "./icon.png", "./icon-512.png"].map(path => new URL(path, self.registration.scope).href);
+const SHELL = ["./", "./index.html", "./portal.css", "./manifest.webmanifest", "./app.css", "./app.js", "./i18n.js", "./sheets-reader.js", "./portal.js", "./lucide.js", "./icon.png", "./icon-512.png", "./privacy.html", "./privacy.css"].map(path => new URL(path, self.registration.scope).href);
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL))));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', event => {
